3439ce80330f2291174558e98e2cc3111db90e6f
Jonah Kollner <jkollner@2-H-5.42heilbronn.de>
2023-09-12 12:15:33 +0200

    added rays

---
+++ src/ray.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 12:10:48 by jkollner          #+#    #+#             */
+/*   Updated: 2023/09/12 12:14:48 by jkollner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAY_H
+# define RAY_H
+
+#include "vec3/vec3.h"
+
+typedef struct s_ray{
+	t_vec3 origin;
+	t_vec3 direction;
+}	t_ray;
+
+t_vec3 p_at(t_vec3 direction, t_vec3 origin, double t);
+
+#endif
