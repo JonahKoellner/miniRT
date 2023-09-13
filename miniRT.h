4f07a0dcffa6d50348a7a266e8519ff6a652b441
Jonah Kollner <jkollner@2-H-5.42heilbronn.de>
2023-09-13 12:03:52 +0200

    made ray into own folder in src/utils/

--- miniRT.h
+++ miniRT.h
@@ -6,7 +6,7 @@
 /*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 09:47:08 by jkollner          #+#    #+#             */
-/*   Updated: 2023/09/13 10:29:39 by jkollner         ###   ########.fr       */
+/*   Updated: 2023/09/13 12:02:42 by jkollner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 # define MINIRT_H
 
 # include "lib/MLX42/include/MLX42/MLX42.h"
-# include "src/ray.h"
+# include "src/utils/ray/ray.h"
 
 #endif
