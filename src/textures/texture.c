5dc286c5085e144985a267652361f3946e78141e
Max Reidenbach <mreidenb@student.42heilbronn.de>
2023-10-12 17:49:49 +0200

    Hit functions using bump mapping

--- src/textures/texture.c
+++ src/textures/texture.c
@@ -6,7 +6,7 @@
 /*   By: mreidenb <mreidenb@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:53:28 by mreidenb          #+#    #+#             */
-/*   Updated: 2023/10/12 00:37:47 by mreidenb         ###   ########.fr       */
+/*   Updated: 2023/10/12 17:47:20 by mreidenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ t_vec3	get_surface_color(t_object *object, t_vec3 point)
 	return (object->mat.color);
 }
 
-t_vec3	get_surface_normal(t_object *object, t_vec3 point, t_vec3 normal)
+t_vec3	get_normal(t_object *object, t_vec3 point, t_vec3 normal)
 {
 	t_vec3	local_normal;
 	t_vec3	tangent;
@@ -50,10 +50,11 @@ t_vec3	get_surface_normal(t_object *object, t_vec3 point, t_vec3 normal)
 		local_normal = texture_get_pixel(object->mat.bump_color.bump, uv.u, uv.v);
 		tangent = cross(normal, (t_vec3){0, 1, 0});
 		bitangent = cross(normal, tangent);
-		tangent_to_world = (t_mat4){{{tangent.x, tangent.y, tangent.z, 0},
-									  {bitangent.x, bitangent.y, bitangent.z, 0},
-									  {normal.x, normal.y, normal.z, 0},
-									  {0, 0, 0, 1}}};
+		tangent_to_world = (t_mat4){{
+		{tangent.x, tangent.y, tangent.z, 0},
+		{bitangent.x, bitangent.y, bitangent.z, 0},
+		{normal.x, normal.y, normal.z, 0},
+		{0, 0, 0, 1}}};
 		local_normal = transform_point(local_normal, tangent_to_world);
 		return (local_normal);
 	}
