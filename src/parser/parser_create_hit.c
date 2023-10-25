9cb37f5cc8da2b532352f80bda3562bfae89ccf3
Jonah Kollner <jkollner@2-A-1.42heilbronn.de>
2023-10-25 16:20:18 +0200

    added bump map niull to plane parser

--- src/parser/parser_create_hit.c
+++ src/parser/parser_create_hit.c
@@ -6,7 +6,7 @@
 /*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:04:00 by jkollner          #+#    #+#             */
-/*   Updated: 2023/10/25 15:59:50 by jkollner         ###   ########.fr       */
+/*   Updated: 2023/10/25 16:20:01 by jkollner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ t_object	create_plane(char *define_line, int *map)
 	obj.obj.plane.origin = fill_vec(split[1], map, -INFINITY, INFINITY);
 	obj.obj.plane.normal = fill_vec(split[2], map, -1, 1);
 	obj.mat.color = (t_vec3){0};
+	obj.mat.bump_color = fill_bumpmap(NULL, NULL);
 	obj.mat.type = METAL;
 	if (ft_veclen(split) == 5)
 		obj.mat.type = fill_material(split[4], map);
@@ -49,9 +50,9 @@ t_object	create_sphere(char *define_line, int *map)
 	obj.mat.type = METAL;
 	if (!num_color(split[3]))
 		obj.mat.color = fill_vec(split[3], map, 0, 255);
-	else if (ft_veclen(split) == 7)
+	if (ft_veclen(split) == 7)
 		obj.mat.bump_color = fill_bumpmap(split[5], split[6]);
-	else if (ft_veclen(split) == 5)
+	if (ft_veclen(split) == 5)
 		obj.mat.type = fill_material(split[4], map);
 	else
 		return (map[OBJ_ERROR]++, ft_vecfree(split), (t_object){});
