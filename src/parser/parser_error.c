2cce71e8f7eefe0dd1d7462213eb356cef7ddb96
Jonah Köllner <koellnjo@gmail.com>
2023-10-29 16:14:11 +0100

    made it mandatory to create camera and ambient light

--- src/parser/parser_error.c
+++ src/parser/parser_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_error.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jonahkollner <jonahkollner@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 15:05:23 by jkollner          #+#    #+#             */
-/*   Updated: 2023/10/27 16:41:05 by jkollner         ###   ########.fr       */
+/*   Updated: 2023/10/29 16:13:49 by jonahkollne      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	check_map(int *map)
 	printf("OBJ_CONE: %d,\n", map[OBJ_CONE]);
 	printf("OBJ_AMBIENT_LIGHT: %d,\n", map[OBJ_AMBIENT_LIGHT]);
 	printf("OBJ_ERROR: %d\n}\n", map[OBJ_ERROR]);
-	if (map[OBJ_CAMERA] > 1 || map[OBJ_AMBIENT_LIGHT] > 1 || map[OBJ_ERROR] > 0)
+	if (map[OBJ_CAMERA] != 1 || map[OBJ_AMBIENT_LIGHT] != 1 || map[OBJ_ERROR] > 0)
 		return (1);
 	return (0);
 }
